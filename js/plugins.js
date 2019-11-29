4c5836ac0ad40ae80486c5140d938d59b4fc83ea
James McGrath <james@jamesmcgrath.net>
2019-11-29 15:53:58 +0900

    add polyfill and remove syntax error

--- js/plugins.js
+++ js/plugins.js
@@ -60,4 +60,13 @@ if (!Element.prototype.closest) {
   };
 }
 
+//forEach Polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function(callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
 // Place any jQuery/helper plugins in here.
