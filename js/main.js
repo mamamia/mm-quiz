75d243ec095ad5318516e26646e0f5b34a38da1d
James McGrath <james@jamesmcgrath.net>
2019-11-29 09:20:28 +0900

    combing dublicate event listener

--- js/main.js
+++ js/main.js
@@ -38,24 +38,22 @@ function checkAnswers(answers) {
       }
     }
   }
-  return results;
+  // return results;
+  return "d";
 }
 
-function showResults(answers, quizLength) {
-  var quizFin = checkQuizDone(answers, quizLength);
-  if (!quizFin) {
-    return;
-  }
-  var results = checkAnswers(answers);
-  // document.querySelector(".result-" + results).classList.add("is-visible");
-  // document.querySelector(".button-reset").classList.add("is-visible");
-  // document.querySelector(".questions").classList.add("is-hidden");
-  // document.querySelector(".hero-image").classList.add("is-hidden");
-  // document.querySelector(".button-prev").classList.add("is-hidden");
-}
+// function showResults(answers, quizLength) {
+//   var quizFin = checkQuizDone(answers, quizLength);
+//   if (!quizFin) {
+//     return;
+//   }
+//   return checkAnswers(answers);
+// }
 
 function resetQuiz(answers) {
   var results = checkAnswers(answers);
+  console.log(results);
+
   var allRadioButtons = document.querySelectorAll('input[type="radio"');
 
   for (var i = 0; i < allRadioButtons.length; i++) {
@@ -64,7 +62,6 @@ function resetQuiz(answers) {
 
   var questions = document.querySelectorAll(".question");
   var allAnswer = document.querySelectorAll(".answer");
-
   timeline["gsap"]
     .to(allAnswer, { duration: 0, x: 0 })
     .to(questions, {
@@ -74,7 +71,8 @@ function resetQuiz(answers) {
     .to(".result-" + results, { duration: 0.5, opacity: 0, zIndex: 0 })
     .to(".button-reset", { duration: 0.5, opacity: 0, zIndex: 0 })
     .to(".questions", { duration: 0.5, opacity: 1 })
-    .to(".hero-image", { duration: 0.5, opacity: 1 });
+    .to(".hero-image", { duration: 0.5, opacity: 1 })
+    .to(".button-prev", { duration: 0.5, opacity: 1 });
 
   for (var i = 0; i < answers.length; i++) {
     answers[i][1] = 0;
@@ -91,12 +89,14 @@ prevButton.addEventListener("click", function(event) {
   timeline["gsap"].reverse();
 });
 
+var timeline = [];
+
 var allRadioInputs = document.querySelectorAll("input[type='radio']");
 allRadioInputs.forEach(function(input) {
   input.addEventListener("click", function(event) {
-    var el = event.target;
-    var answer = parseInt(el.value);
-    var question = el.closest("fieldset");
+    var selected = event.target;
+    var answer = parseInt(selected.value);
+    var question = selected.closest("fieldset");
 
     if (question.getAttribute("data-selected")) {
       answers[question.getAttribute("data-selected")][1] -= 1;
@@ -104,15 +104,9 @@ allRadioInputs.forEach(function(input) {
 
     answers[answer][1] += 1;
     question.setAttribute("data-selected", answer);
-    showResults(answers, quizLength);
-  });
-});
+    // showResults(answers, quizLength);
 
-var timeline = [];
-var allInputs = document.querySelectorAll("input[type='radio']");
-allInputs.forEach(function(input, index) {
-  input.addEventListener("click", function(event) {
-    var selected = event.target;
+    // var selected = event.target;
     var selectedParent = selected.parentElement;
     var selectedName = selected.getAttribute("name");
     var quizNumber = selectedName.split("-")[1];
@@ -141,12 +135,55 @@ allInputs.forEach(function(input, index) {
     } else {
       var results = checkAnswers(answers);
       timeline["gsap"]
+        .to(".button-prev", { duration: 0, opacity: 0 }, 0)
         .to(".questions", { duration: 0.5, opacity: 0 }, 0)
         .to(".hero-image", { duration: 0.5, opacity: 0 }, 0)
         .to(".result-" + results, { duration: 0.5, opacity: 1, zIndex: 1 }, 0.5)
         .to(".button-reset", { duration: 0.5, opacity: 1, zIndex: 1 }, 0.5);
       console.log("end");
     }
-    console.log(timeline);
   });
 });
+
+// var allInputs = document.querySelectorAll("input[type='radio']");
+// allInputs.forEach(function(input, index) {
+//   input.addEventListener("click", function(event) {
+//     var selected = event.target;
+//     var selectedParent = selected.parentElement;
+//     var selectedName = selected.getAttribute("name");
+//     var quizNumber = selectedName.split("-")[1];
+//     var allLabels = document.querySelectorAll(
+//       '[data-label="' + selectedName + '"]'
+//     );
+//     var questions = document.querySelectorAll(".question");
+
+//     var labels = [];
+//     allLabels.forEach(function(el) {
+//       if (el === selectedParent) {
+//         console.log("MATCH", el, selectedParent);
+//         return;
+//       }
+//       labels.push(el);
+//     });
+
+//     timeline["gsap"] = gsap.timeline();
+//     if (quizNumber != questions.length) {
+//       timeline["gsap"]
+//         .to(labels, { duration: 1, x: "-120%", stagger: 0.2 }, selectedName)
+//         .to(questions, {
+//           duration: 1,
+//           x: "-" + quizNumber * 100 + "%"
+//         });
+//     } else {
+//       var results = checkAnswers(answers);
+//       timeline["gsap"]
+//         .to(".button-prev", { duration: 0, opacity: 0 }, 0)
+//         .to(".questions", { duration: 0.5, opacity: 0 }, 0)
+//         .to(".hero-image", { duration: 0.5, opacity: 0 }, 0)
+//         .to(".result-" + results, { duration: 0.5, opacity: 1, zIndex: 1 }, 0.5)
+//         .to(".button-reset", { duration: 0.5, opacity: 1, zIndex: 1 }, 0.5);
+//       console.log("end");
+//     }
+//     console.log(timeline);
+//   });
+// });
